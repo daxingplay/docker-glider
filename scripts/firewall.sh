d903f04560e656396ff6aabeb997a7ed061b03b4
daxingplay <daxingplay@gmail.com>
2018-10-20 15:52:59 +0800

    feat: use new iptable rule

--- scripts/firewall.sh
+++ scripts/firewall.sh
@@ -10,18 +10,21 @@ fw_setup() {
 
   iptables -P INPUT ACCEPT
   iptables -P FORWARD ACCEPT
+  iptables -t nat -N GLIDER
 
   #iptables -t nat -A POSTROUTING -s 192.168.0.0/24 -j MASQUERADE
 
-  iptables -t nat -I PREROUTING -p tcp -m set --match-set glider dst -j REDIRECT --to-ports 1081
-  iptables -t nat -I OUTPUT -p tcp -m set --match-set glider dst -j REDIRECT --to-ports 1081
+#   iptables -t nat -I PREROUTING -p tcp -m set --match-set glider dst -j REDIRECT --to-ports 1081
+#   iptables -t nat -I OUTPUT -p tcp -m set --match-set glider dst -j REDIRECT --to-ports 1081
+  iptables -t nat -A GLIDER -p tcp -j REDIRECT --to-ports 1081
+  iptables -t nat -A PREROUTING -p tcp -j GLIDER
 }
 
 ##########################
 # Clear the Firewall rules
 ##########################
 fw_clear() {
-  iptables-save | grep -v glider | iptables-restore
+  iptables-save | grep -v GLIDER | iptables-restore
   ipset destroy glider
   #iptables -L -t nat --line-numbers
   #iptables -t nat -D PREROUTING 2
